f1c8e621c585995c763ca1633d486d0e0824c14c
Charles Bennett <cfbennet@usc.edu>
2019-03-23 16:00:26 -0700

    forgot to save last time

--- p1.c
+++ p1.c
@@ -11,26 +11,37 @@ int main(int argc, char** argv)
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	int MSG, flag = 0;
+	void* MSG_buffer;
 
 	if (rank == 0)
 	{
 		if (flag)
 		{
 			MSG = 451;
-			printf("Process %d, Initially Message = %d", rank, MSG);
+			printf("Process %d: Initially Message = %d", rank, MSG);
+			MSG_buffer = &MSG;
+			MPI_send(MSG_buffer, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+		}
+		else
+		{
+			MPI_Recv(MSG_buffer, 1, MPI_INT, 3, 3, MPI_COMM_WORLD);
+			MSG = (int) (*MSG_buffer);
+			printf("Process %d: Recieved Message = %d Done!", rank, MSG)
 		}
 	}
 
 	else
 	{
-
+		MPI_Recv(MSG_buffer, 1, MPI_INT, (rank - 1), (rank - 1), MPI_COMM_WORLD);
+		MSG = (int) (*MSG_buffer);
 		MSG++;
-		printf("Process %d, Message = %d", rank, MSG);
+		printf("Process %d: Message = %d", rank, MSG);
+		MSG_buffer = &MSG;
+		MPI_send(MSG_buffer, 1, MPI_INT, (rank + 1), rank, MPI_COMM_WORLD);
 	}
 
-	MPI_Scatter(a, 1, MPI_INT, &b, 1, MPI_INT, 2, MPI_COMM_WORLD);
 
-	printf("Process %d: %d \n",  rank, b);
+
 
 	MPI_Finalize();
 	return 0;
