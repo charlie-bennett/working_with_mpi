ad83cec95c58254b0ff5e23892d5beaefd3e4f46
Charles Bennett <cfbennet@usc.edu>
2019-03-23 17:12:51 -0700

    gutted use of count variable

--- p1.c
+++ p1.c
@@ -11,44 +11,29 @@ int main(int argc, char** argv)
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	int MSG, dest, flag = 0;
-	int count = 0;
+	//int count = 0;
 	void* MSG_buffer;
 
-	if (rank == 0 && count == 0)
+	if (rank == 0)
 	{
-		if (flag == 0)
-		{
-			MSG = 451;
-			printf("Process %d: Initially Message = %d \n", rank, MSG);
-			MPI_Send(&MSG, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
 
-		}
-		else
-		{
-			MPI_Recv(&MSG, 1, MPI_INT, 3, 3, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-			printf("Process %d: Recieved Message = %d Done! \n", rank, MSG);
-			return 0;
-		}
-		count++;
+		MSG = 451;
+		printf("Process %d: Initially Message = %d \n", rank, MSG);
+		MPI_Send(&MSG, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+
+		MPI_Recv(&MSG, 1, MPI_INT, 3, 3, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		printf("Process %d: Recieved Message = %d Done! \n", rank, MSG);
+
+
 
 	}
 
-	else if (rank == count)
+	else
 	{
 		MPI_Recv(&MSG, 1, MPI_INT, (rank - 1), (rank - 1), MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 		MSG++;
 		printf("Process %d: Message = %d \n", rank, MSG);
-		if (rank == 3)
-		{
-			count = 0;
-			dest = 0;
-			flag = 1;
-		}
-		else
-		{
-			dest = (rank + 1);
-			count++;
-		}
+		dest = (rank == 3) ? 0 : (rank + 1);
 
 		MPI_Send(&MSG, 1, MPI_INT, dest, rank, MPI_COMM_WORLD);
 
