d5c6ee3487f479a472eae9a9c12f530967432bfc
Charles Bennett <cfbennet@usc.edu>
2019-03-23 21:11:25 -0700

    lets keep going

--- p2_2.c
+++ p2_2.c
@@ -9,16 +9,8 @@
 int read_file(int* buffer)
 {
 
-	MPI_File my_file;
-	MPI_Status status;
-	int rc = MPI_File_open(MPI_COMM_WORLD, "number.txt", MPI_MODE_RDONLY, MPI_INT, &my_file);
-	printf("rc is %d \n", rc);
-
-	rc = MPI_File_read(my_file, buffer, buffer_size, MPI_INT, &status);
-	printf("rc is %d \n", rc);
-	/*
-
-	//if (my_file == NULL) {return 1;}
+	FILE* my_file = fopen ("number.txt", "r");
+	if (my_file == NULL) {return 1;}
 	int index = 0;
 	fscanf(my_file, "%d", (buffer + index));
 	while ((!(feof(my_file))) && index < buffer_size)
@@ -26,10 +18,7 @@ int read_file(int* buffer)
 		index++;
 		fscanf(my_file, "%d", (buffer + index));
 	}
-
-	*/
-
-	MPI_File_close(&my_file);
+	fclose(my_file);
 
 	return 0;
 }
@@ -51,35 +40,33 @@ int main(int argc, char** argv)
 	int end_index = start_index + (buffer_size / size);
 
 
-	read_file(buffer);
 
 
+
+
+
+
+
+	if (rank == 0)
+	{
+
+		read_file(buffer);
+		MPI_Bcast(buffer, buffer_size, MPI_INT, 0, MPI_COMM_WORLD);
+
+		runsum += runsum_buff;
+		printf("Sum is %d \n", runsum);
+	}
 
 	for (i = start_index; i < end_index; i++)
 	{
 		runsum = runsum + buffer[i];
 	}
 
+	MPI_Reduce(&runsum, &runsum_buff, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
 	if (rank == 0)
 	{
-
-
-		/*
-		for (i = 1; i < 4; i++)
-		{
-			MPI_Recv(&runsum_buff, 1, MPI_INT, i, i, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-			runsum += runsum_buff;
-			printf("we are at this i = %d", i);
-		}
-		*/
-		MPI_Recv(&runsum_buff, 1, MPI_INT, 1, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-		runsum += runsum_buff;
-		printf("Sum is %d \n", runsum);
-	}
-	else if (rank == 1)
-	{
-		MPI_Send(&runsum, 1, MPI_INT, 0, rank, MPI_COMM_WORLD);
+		fprint("The sum is ", runsum_buff);
 	}
 
 
