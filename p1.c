876ef8dbd70afb08f6f8d4c35d4ccb7fbc34e36a
Charles Bennett <cfbennet@usc.edu>
2019-04-02 19:32:49 -0700

    keep on going

--- p1.c
+++ p1.c
@@ -16,44 +16,42 @@ int main(int argc, char** argv)
 	int q, i, old_q;
 	int recieve_from, send_to;
 	MSG = 0;
+	old_q = 0;
 
 	for (q = 1; q < size; q++)
 	{
-		for (i = 0; i < size; i++)
+
+		printf("Process %d: Initially Message = %d \n", rank, MSG);
+		recieve_from = rank - q;
+		if (q < 0)
 		{
-			if (rank == i)
-			{
-				printf("Process %d: Initially Message = %d \n", rank, MSG);
-				recieve_from = i - q;
-				if (q < 0)
-				{
-					recieve_from = recieve_from + size;
-				}
-				send_to = ((i + q) % size);
-				if (i == 0)
-				{
-					if (old_q < q)
-					{
-						recieve_from = 8 - old_q;
-						MPI_Recv(&MSG, 1, MPI_INT, recieve_from, recieve_from, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-						printf("Process %d: Recieved Message = %d Done! \n", rank, MSG);
-					}
-
-					printf("Process %d: Initially Message = %d \n", rank, MSG);
-					MPI_Send(&MSG, 1, MPI_INT, send_to, rank, MPI_COMM_WORLD);
-					old_q = q;
-
-				}
-				else
-				{
-					MPI_Recv(&MSG, 1, MPI_INT, recieve_from, recieve_from, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-					MSG++;
-					printf("Process %d: Recieved Message = %d Done! \n", rank, MSG);
-					MPI_Send(&MSG, 1, MPI_INT, send_to, rank, MPI_COMM_WORLD);
-				}
-
-			}
+			recieve_from = recieve_from + size;
 		}
+		send_to = ((rank + q) % size);
+		if (rank == 0)
+		{
+			if (old_q < q)
+			{
+				recieve_from = 8 - old_q;
+				MPI_Recv(&MSG, 1, MPI_INT, recieve_from, recieve_from, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+				printf("Process %d: Recieved Message = %d Done! \n", rank, MSG);
+			}
+
+			printf("Process %d: Initially Message = %d \n", rank, MSG);
+			MPI_Send(&MSG, 1, MPI_INT, send_to, rank, MPI_COMM_WORLD);
+			old_q = q;
+
+		}
+		else
+		{
+			MPI_Recv(&MSG, 1, MPI_INT, recieve_from, recieve_from, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			MSG++;
+			printf("Process %d: Recieved Message = %d Done! \n", rank, MSG);
+			MPI_Send(&MSG, 1, MPI_INT, send_to, rank, MPI_COMM_WORLD);
+		}
+
+
+
 	}
 
 
